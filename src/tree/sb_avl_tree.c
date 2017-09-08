625539df9af87bab1b4dd2ccaf3868a81e2d8d04
cui shibing <643237029@qq.com>
2017-09-09 00:01:52 +0800

    修改文件名

--- src/tree/sb_avl_tree.c
+++ src/tree/sb_avl_tree.c
@@ -1,223 +1,116 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <assert.h>
-//#include "sb_tree.h"
-//#include "sb_collection.h"
 //
-///*
-// * 返回parent_node节点是否平衡
-// * big_node为高度更高的节点
-// * */
-//int is_balance(sb_tree_node *parent_node,sb_tree_node **big_node);
+// Created by cui on 17-9-8.
 //
-///**
-// * 以parent_node节点进行左旋转
-// * @param parent_node 新插入节点的祖父节点
-// * */
-//void left_left_rotate(sb_tree_node *parent_node);
-//
-//void right_right_rotate(sb_tree_node *parent_node);
-//
-//int sb_init_avl_tree(sb_avl_tree *bst){
-//    if(bst==NULL)
-//        return 0;
-//    //初始情况下让根节点的儿子指向自己,根据这个条件来判断根节点还未初始化
-//    bst->node1 = bst;
-//    bst->node2 = bst;
-//    bst->parent = NULL;
-//    bst->height = -1;
-//    return 1;
-//}
-//
-//int sb_insert_avl_tree(sb_avl_tree *bst,sb_tree_node node_){
-//    assert(bst!=NULL);
-//    node_.node1 = NULL;
-//    node_.node2 = NULL;
-//    if(bst->node1==bst&&bst->node2==bst){
-//        *bst = node_;
-//        bst->parent = NULL;
-//        bst->height = 0;
-//        return 1;
-//    }
-//    sb_tree_node *node = (sb_tree_node*)malloc(sizeof(sb_tree_node));
-//    if(node==NULL)
-//        return 0;
-//    *node = node_;
-//
-//    sb_stack node_stack;
-//    sb_element e;
-//    if(!sb_init_stack(&node_stack,12))
-//        return 0;
-//    e.value = bst;
-//    sb_push_stack(&node_stack,e);
-//    sb_tree_node *iteator = bst;
-//    while(1){
-//        if(sb_compare(node->e, iteator->e)<0){//小于根结点
-//            if(iteator->node1!=NULL){//有左子树
-//                iteator = iteator->node1;
-//                e.value = iteator;
-//                sb_push_stack(&node_stack,e);
-//                continue;
-//            }else{
-//                iteator->node1 = node;
-//                iteator->node1->parent = iteator;
-//
-//                while (node_stack.length>0){
-//                    sb_pop_stack(&node_stack,&e);
-//                    sb_tree_node *tmp_node = NULL;
-//                    if(!is_balance((sb_tree_node*)e.value,&tmp_node)){//不平衡了,且不平衡的节点是:tmp_node
-//                        if(tmp_node->parent->node1==tmp_node){//L,tmm_node是个左节点
-//                            if(tmp_node->node1==NULL){//LR
-//                                right_right_rotate(tmp_node->node2);
-//                                left_left_rotate(tmp_node);
-//                            }else if(tmp_node->node2==NULL){//LL
-//                                left_left_rotate(tmp_node);
-//                            }else{
-//                                if(tmp_node->node1->height > tmp_node->node2->height){//LL
-//                                    left_left_rotate(tmp_node);
-//                                } else{//LR
-//                                    right_right_rotate(tmp_node->node2);
-//                                    left_left_rotate(tmp_node);
-//                                }
-//                            }
-//                        } else{//R
-//                            if(tmp_node->node1==NULL){//RR
-//                                right_right_rotate(tmp_node);
-//                            }else if(tmp_node->node2==NULL){//RL
-//                                left_left_rotate(tmp_node->node1);
-//                                right_right_rotate(tmp_node);
-//                            }else {
-//                                if(tmp_node->node1->height > tmp_node->node2->height){//RL
-//                                    left_left_rotate(tmp_node->node1);
-//                                    right_right_rotate(tmp_node);
-//                                } else{//RR
-//                                    right_right_rotate(tmp_node);
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//                break;
-//            }
-//        }else{//大于根结点
-//            if(iteator->node2!=NULL){//有右子树
-//                iteator = iteator->node2;
-//                e.value = iteator;
-//                sb_push_stack(&node_stack,e);
-//                continue;
-//            }else{
-//                iteator->node2 = node;
-//                iteator->node2->parent = iteator;
-//                while (node_stack.length>0){
-//                    sb_pop_stack(&node_stack,&e);
-//                    sb_tree_node *tmp_node = NULL;
-//                    if(!is_balance((sb_tree_node*)e.value,&tmp_node)){//不平衡了
-//                        if(tmp_node->parent->node1==tmp_node){//L
-//                            if(tmp_node->node1==NULL){//LR
-//                                right_right_rotate(tmp_node->node2);
-//                                left_left_rotate(tmp_node);
-//                            }else if(tmp_node->node2==NULL){//LL
-//                                left_left_rotate(tmp_node);
-//                            }else{
-//                                if(tmp_node->node1->height > tmp_node->node2->height){//LL
-//                                    left_left_rotate(tmp_node);
-//                                } else{//LR
-//                                    right_right_rotate(tmp_node->node2);
-//                                    left_left_rotate(tmp_node);
-//                                }
-//                            }
-//                        } else{//R
-//                            if(tmp_node->node1==NULL){//RR
-//                                right_right_rotate(tmp_node);
-//                            }else if(tmp_node->node2==NULL){//RL
-//                                left_left_rotate(tmp_node->node1);
-//                                right_right_rotate(tmp_node);
-//                            }else {
-//                                if(tmp_node->node1->height > tmp_node->node2->height){//RL
-//                                    left_left_rotate(tmp_node->node1);
-//                                    right_right_rotate(tmp_node);
-//                                } else{//RR
-//                                    right_right_rotate(tmp_node);
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//                break;
-//            }
-//        }
-//    }
-//    sb_clear_stack(&node_stack);
-//    return 1;
-//}
-//int sb_find_min_avl_tree(sb_tree_node *tr,sb_tree_node **tn);
-//int sb_find_max_avl_tree(sb_tree_node *tr,sb_tree_node **tn);
-//int sb_get_avl_tree(sb_avl_tree *bst,int key,sb_tree_node **node_);
-//int sb_traversal_avl_tree(sb_avl_tree *bst,arraylist *list,sb_traversal_type bt);
-//int sb_remove_avl_tree(sb_avl_tree *bst,int key,sb_tree_node *node);
-//
-//int calculate_tree_height(sb_tree_node *node){
-//    if (node==NULL){
-//        return -1;
-//    }
-//    int node1_height = calculate_tree_height(node->node1);
-//    int node2_height = calculate_tree_height(node->node2);
-//    return (node->height = 1 + (node1_height > node2_height ? node1_height : node2_height));
-//}
-//
-//int is_balance(sb_tree_node *parent_node,sb_tree_node **big_node){
-//    assert(parent_node!=NULL&&big_node!=NULL);
-//    int node1_height =
-//            calculate_tree_height(parent_node->node1);
-//    int node2_height =
-//            calculate_tree_height(parent_node->node2);
-//    if(abs(node1_height-node2_height)>1){//不平衡了
-//        node1_height>node2_height?((*big_node)=parent_node->node1):((*big_node)=parent_node->node2);
-//        return 0;
-//    }
-//    return 1;
-//}
-//
-//void left_left_rotate(sb_tree_node *parent_node){
-//    sb_tree_node *pre_root_node = (sb_tree_node*)malloc(sizeof(sb_tree_node));
-//    sb_tree_node *tem_node = NULL;
-//    *pre_root_node = *(parent_node->parent);
-//
-//    *(parent_node->parent) = *(parent_node);
-//    tem_node = parent_node->parent;
-//    free(parent_node);
-//    parent_node = tem_node;
-//    parent_node->parent = pre_root_node->parent; //parent
-//    if(parent_node->node1!=NULL)
-//        parent_node->node1->parent = parent_node;//要重新设置父亲节点,因为父节点的位置已经改变
-//
-//    pre_root_node->node1 = parent_node->node2;
-//    if(pre_root_node->node1!=NULL)
-//        pre_root_node->node1->parent = pre_root_node;
-//
-//    parent_node->node2 = pre_root_node;
-//    if(parent_node->node2!=NULL)
-//        parent_node->node2->parent = parent_node;
-//}
-//
-//void right_right_rotate(sb_tree_node *parent_node){
-//    sb_tree_node *tem_node = NULL;
-//    sb_tree_node *pre_root_node = (sb_tree_node*)malloc(sizeof(sb_tree_node));
-//    *pre_root_node = *(parent_node->parent);
-//
-//    *(parent_node->parent) = *(parent_node);
-//    tem_node = parent_node->parent;
-//    free(parent_node);
-//    parent_node = tem_node;
-//    parent_node->parent = pre_root_node->parent;
-//    if(parent_node->node2!=NULL)
-//        parent_node->node2->parent = parent_node;//要重新设置父亲节点,因为父节点的位置已经改变
-//
-//    pre_root_node->node2 = parent_node->node1;
-//    if(pre_root_node->node2!=NULL)
-//        pre_root_node->node2->parent = pre_root_node;
-//
-//    parent_node->node1 = pre_root_node;
-//    if(parent_node->node1!=NULL)
-//        parent_node->node1->parent = parent_node;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "sb_tree.h"
+sb_tree_node* insert_avl_tree(sb_tree_node **root,sb_tree_node *data);
+
+sb_tree_node* left_left_rotate(sb_tree_node *root);
+sb_tree_node* right_right_rotate(sb_tree_node *root);
+
+int height(sb_tree_node *node);
+
+int sb_insert_avl_tree(sb_avl_tree **root,sb_tree_node node_){
+    if(root==NULL)
+        return 0;
+    node_.node1 = node_.node2 = NULL;
+    node_.height = 0;
+    sb_tree_node *result = insert_avl_tree(root,&node_);
+    return result==NULL?0:1;
+}
+
+sb_tree_node* insert_avl_tree(sb_tree_node **root,sb_tree_node *data){
+    if (*root == NULL){
+        *root = (sb_tree_node*)malloc(sizeof(sb_tree_node));
+        if(*root == NULL)
+            return NULL;
+        **root = *data;
+        return *root;
+    }
+    int compare_result = sb_compare(data->e,(*root)->e);
+    if(compare_result<0){
+        if(insert_avl_tree(&(*root)->node1,data)){
+            if(height((*root)->node1)-height((*root)->node2)>1){
+                compare_result = sb_compare(data->e,(*root)->node1->e);
+                if(compare_result<0){//LL
+                    *root = left_left_rotate(*root);
+
+                }
+                if(compare_result>0){//LR
+                    (*root)->node2 = right_right_rotate((*root)->node2);
+                    *root = left_left_rotate(*root);
+
+                }
+            }
+        }else{
+            return NULL;
+        }
+    }
+
+    if (compare_result>0){
+        if(insert_avl_tree(&(*root)->node2,data)){
+            if(height((*root)->node2)-height((*root)->node1)>1){
+                compare_result = sb_compare(data->e,(*root)->node2->e);
+                if(compare_result>0){//RR
+                    *root = right_right_rotate(*root);
+
+                }
+                if(compare_result<0){//RL
+                    (*root)->node1 = left_left_rotate((*root)->node1);
+                    *root = right_right_rotate(*root);
+
+                }
+            }
+        }else{
+            return NULL;
+        }
+    }
+
+    if (compare_result==0){
+
+    }
+    height(*root);
+    return *root;
+}
+int height(sb_tree_node *node){
+    if(node==NULL){
+        return -1;
+    }
+    int left_height = -1;
+    int right_height = -1;
+    if(node->node1!=NULL)
+        left_height = node->node1->height;
+    if(node->node2!=NULL)
+        right_height = node->node2->height;
+    return (node->height = (left_height > right_height ? left_height : right_height)+1);
+}
+sb_tree_node* left_left_rotate(sb_tree_node *old_root){
+    sb_tree_node * new_root = old_root->node1;
+    old_root->node1 = new_root->node2;
+    new_root->node2 = old_root;
+    return new_root;
+}
+sb_tree_node* right_right_rotate(sb_tree_node *old_root){
+    sb_tree_node * new_root = old_root->node2;
+    old_root->node2 = new_root->node1;
+    new_root->node1 = old_root;
+    return new_root;
+}
+
+int sb_find_min_avl_tree(sb_avl_tree *tree,sb_tree_node **tn){
+    return sb_find_min_bs_tree(tree,tn);
+}
+int sb_find_max_avl_tree(sb_avl_tree *tree,sb_tree_node **tn){
+    return sb_find_max_bs_tree(tree,tn);
+}
+int sb_get_avl_tree(sb_avl_tree *tree,int key,sb_tree_node **node_){
+    return sb_get_bs_tree(tree,key,node_);
+}
+int sb_traversal_avl_tree(sb_avl_tree *tree,arraylist *list,sb_traversal_type bt){
+    return sb_traversal_bs_tree(tree,list,bt);
+}
+int sb_remove_avl_tree(sb_avl_tree **tree,int key,sb_tree_node *node){
+    return 1;
+}
