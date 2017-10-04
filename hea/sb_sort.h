3f478b471b95a509b08b5122eeaf103b81ffe735
cui shibing <643237029@qq.com>
2017-10-04 11:17:36 +0800

    排序头文件的注释

--- hea/sb_sort.h
+++ hea/sb_sort.h
@@ -3,22 +3,41 @@
 
 #include "sb_base.h"
 
-/*
-  常用的排序算法
-*/
+/**
+ * 常用的排序算法
+ * */
+
+/**
+ * 插入排序
+ * */
 void sb_insert_sort(sb_element *array,int length);
 
+/**
+ * 归并排序
+ * */
 void sb_merge_sort(sb_element *array,int length);
 
+/**
+ * 快速排序
+ * */
 void sb_quick_sort(sb_element *array,int length);
 
+/**
+ * 选择排序的两种选择极值的方法
+ * */
 typedef enum {
   LINEAR,//使用线性查找查找极值
   HEAP//使用堆结构查找极值
 }TYPE;
 
+/**
+ * 选择排序
+ * */
 void sb_select_sort(sb_element *array,int length,TYPE type);
 
+/**
+ * shell排序
+ * */
 void sb_shell_sort(sb_element *array,int length,int *increments,int increment_length);
 
 #endif
